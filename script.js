012953124dd97177ba4859047f067eae00daeef6
fouziah ahmed <fouziahahmed@fouziahs-MacBook-Air.local>
2023-10-17 01:01:50 -0500

    added onclick

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 $(document).ready(function(){
     function formData(event){
-    event.preventDefault()
-    var formSerial = $('#task-form').serializeArray();
-    $('#to-do-list').append(formSerial);
-}
-
-    // $('button').on('click', function(){
-    //     var inp = $("input:text").val();
-    //     var newItem = $("<li>").text(inp);
-    //     var ulElement = $("<ul>");
-    //     $('ul').append(li)
-    //     $('#to-do-list').append(ulElement);
-    // })
-    $('button').on('click', function(){
-        var inp = $("input:text").val();
-        var newItem = $("<li>").text(inp);
-        $('ul').append(newItem);
-        $('#to-do-list').append($('ul'));
-        $("input:text").val('');
-    })
+        event.preventDefault();
+        var listArray = $('form').serializeArray();
+        console.log(listArray);
+        var taskInput = listArray[0].value
+        $('#to-do-list').append('<li>' + taskInput + "</li>");
+        
+    }
+    });
     
 
-})
